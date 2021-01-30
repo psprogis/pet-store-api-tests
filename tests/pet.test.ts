fa2a92e76eac64cd768b64e034aa26562dd410b9
Pavlo Sprogis <pavel.sprogis@gmail.com>
2021-01-30 14:20:51 +0200

    uncomment multiple status check

--- tests/pet.test.ts
+++ tests/pet.test.ts
@@ -22,12 +22,12 @@ describe('User can', function() {
         assert(petResp.length > 0, 'no sold')
 
         // Multiple statuses are applicable as well
-        // petResp = await pet.findByStatus(['pending', 'available']);
-        // assert(petResp.length > 0)
+        petResp = await pet.findByStatus(['pending', 'available']);
+        assert(petResp.length > 0)
 
-        // assert(petResp.some((pet: any) => pet.status == 'available'));
-        // assert(petResp.some((pet: any) => pet.status == 'pending'));
-        // assert(!petResp.some((pet: any) => pet.status == 'sold'));
+        assert(petResp.some((pet: any) => pet.status == 'available'));
+        assert(petResp.some((pet: any) => pet.status == 'pending'));
+        assert(!petResp.some((pet: any) => pet.status == 'sold'));
     });
 
     it('can be received by tag', async function() {
