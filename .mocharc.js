81e809e18d5821558a1409bda29ef40bd8b3c6c3
Pavlo Sprogis <pavel.sprogis@gmail.com>
2021-06-24 13:56:42 +0300

    add allure report with basic features

--- .mocharc.js
+++ .mocharc.js
@@ -6,4 +6,6 @@ module.exports = {
     spec: './tests/**/*.test.ts',
     slow: 50000,
     timeout: 60000,
+    reporter: 'mocha-multi-reporters',
+    reporterOptions: ['configFile=reporterConfig.json']
 }
